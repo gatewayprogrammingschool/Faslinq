fa07ecad6b13590e30af31778d7a38d4c316692c
The Sharp Ninja <ninja@thesharp.ninja>
2021-10-06 05:19:25 -0500

    Clean up generated markdown.

--- bench.ps1
+++ bench.ps1
@@ -9,7 +9,7 @@ param(
     [switch]$Once = $false,
     [switch]$Join = $false,
     [switch]$NoFaslinq = $false,
-    [string[]]$Runtimes = @("net6.0")
+    [string[]]$Runtimes = @('net6.0')
 )
 
 
@@ -33,104 +33,76 @@ function Clean-Folder {
     | Remove-Item -Recurse:$R -Force:$F -Verbose:$V
 
 }
-
-[System.Collections.Generic.List[string[]]]$Names = @()
-if ($All) {
-    [string[]]$Filter = @('*')
-}
-else {
-    $f = @()
-
-    if ($Select) {
-        $f += '-Select'
+try {
+    [System.Collections.Generic.List[string[]]]$Names = @()
+    if ($All) {
+        [string[]]$Filter = @('*')
     }
-    if ($Where) {
-        $f += '-Where'
+    else {
+        $f = @()
+
+        if ($Select) {
+            $f += '-Select'
+        }
+        if ($Where) {
+            $f += '-Where'
+        }
+        if ($Take) {
+            $f += '-Take'
+        }
+        if ($TakeLast) {
+            $f += '-TakeLast'
+        }
     }
-    if ($Take) {
-        $f += '-Take'
+
+    $FF = [System.String]::Join(' ', $f)
+
+    if ($Once) {
+        $OneIteration = '--runOncePerIteration'
     }
-    if ($TakeLast) {
-        $f += '-TakeLast'
+
+    if ($Join) {
+        $joinParameter = '--join'
     }
-}
 
-$FF = [System.String]::Join(' ', $f)
+    if ($NoFaslinq) {
+        $NoFaslinqOption = '/p:DefineConstants=NO_FASLINQ'
+    }
+    else {
+        $NoFaslinqOption = ''
+    }
 
-if ($Once) {
-    $OneIteration = '--runOncePerIteration'
-}
+    Push-Location
 
-if ($Join) {
-    $joinParameter = '--join'
-}
+    $root = Get-Location
 
-if ($NoFaslinq) {
-    $NoFaslinqOption = '/p:DefineConstants=NO_FASLINQ'
-}
-else {
-    $NoFaslinqOption = ''
-}
+    Clean-Folder -p @('bin', 'obj', '*.artifacts') -r -f
 
-Push-Location
-
-$root = Get-Location
-
-Clean-Folder -p @('bin', 'obj', '*.artifacts') -r -f
-
-& dotnet restore
-
-Set-Location "$root\Faslinq.Benchmarks\"
-
-$Runtime = [System.DateTimeOffset]::Now.ToString('G')
-$md = @("# Benchmark Run at ${Runtime}", '')
-
-$Title = 'Faslinq'
-
-if ($NoFaslinq) {
-    $Title = 'Linq'
-}
-
-$md += ''
-$md += "``````powershell"
-$md += "dotnet publish -c $Configuration $NoFaslinqOption -f net6.0 -a x64 --self-contained"
-$md += "``````"
-$md += ''
-& dotnet publish -c $Configuration $NoFaslinqOption -f net6.0 -a x64 --self-contained
-
-Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\"
-
-Write-Verbose "`$Configuration: $Configuration"
-Write-Verbose "`$OneIteration: $OneIteration"
-Write-Verbose "`$Filter: $Filter"
-
-$md += ''
-$md += "``````powershell"
-$md += "& .\Faslinq.Benchmarks.exe $OneIteration $joinParameter ${FF} --platform X64"
-$md += "``````"
-$md += ''
-& .\Faslinq.Benchmarks.exe $OneIteration $joinParameter ${FF} --platform X64
-
-Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\BenchmarkDotNet.Artifacts\results"
-
-$mds = Get-ChildItem '*github.md' -ErrorAction Stop
-
-$md += '', "## ${Title}", ''
-$md += $mds | Get-Content
-
-if ($NoFaslinq) {
-    Remove-Item *github.md
+    & dotnet restore
 
     Set-Location "$root\Faslinq.Benchmarks\"
 
+    $Runtime = [System.DateTimeOffset]::Now.ToString('G')
+    $md = @("# Benchmark Run at ${Runtime}", '')
+
+    $Title = 'Faslinq'
+
+    if ($NoFaslinq) {
+        $Title = 'Linq'
+    }
+
     $md += ''
     $md += "``````powershell"
-    $md += "dotnet publish -c $Configuration -f net6.0 -a x64 --self-contained"
+    $md += "dotnet publish -c $Configuration $NoFaslinqOption -f net6.0 -a x64 --self-contained"
     $md += "``````"
     $md += ''
-    & dotnet publish -c $Configuration -f net6.0 -a x64 --self-contained
+    & dotnet publish -c $Configuration $NoFaslinqOption -f net6.0 -a x64 --self-contained
 
     Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\"
+
+    Write-Verbose "`$Configuration: $Configuration"
+    Write-Verbose "`$OneIteration: $OneIteration"
+    Write-Verbose "`$Filter: $Filter"
 
     $md += ''
     $md += "``````powershell"
@@ -141,30 +113,64 @@ if ($NoFaslinq) {
 
     Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\BenchmarkDotNet.Artifacts\results"
 
-    $md += '', '## Faslinq', ''
-
     $mds = Get-ChildItem '*github.md' -ErrorAction Stop
+
+    $md += '', "## ${Title}", ''
     $md += $mds | Get-Content
+
+    if ($NoFaslinq) {
+        Remove-Item *github.md
+
+        Set-Location "$root\Faslinq.Benchmarks\"
+
+        $md += ''
+        $md += "``````powershell"
+        $md += "dotnet publish -c $Configuration -f net6.0 -a x64 --self-contained"
+        $md += "``````"
+        & dotnet publish -c $Configuration -f net6.0 -a x64 --self-contained
+
+        Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\"
+
+        $md += ''
+        $md += "``````powershell"
+        $md += "& .\Faslinq.Benchmarks.exe $OneIteration $joinParameter ${FF} --platform X64"
+        $md += "``````"
+        & .\Faslinq.Benchmarks.exe $OneIteration $joinParameter ${FF} --platform X64
+
+        Set-Location "$root\Faslinq.Benchmarks\bin\$Configuration\net6.0\win-x64\publish\BenchmarkDotNet.Artifacts\results"
+
+        $md += ''
+
+        $mds = Get-ChildItem '*github.md' -ErrorAction Stop
+
+        for ($i = 0; $i -lt $mds.Count; $i += 1) {
+            $line = $mds[$i]
+            if ($line -eq "``````") {
+                $mds[$i] = $line + [System.Environment]::NewLine
+            }
+        }
+
+        $md += $mds | Get-Content
+    }
+
+    foreach ($name in $Names) {
+        $Key = $name[0] + '|'
+        $Value = $name[1] + '|'
+        $md = $md.Replace($Key, $Value)
+    }
+
+    Write-Host $md
+
+    $resultsFile = "$root\results.md"
+
+    $md | Out-File $resultsFile -Force
+
+    $edge = Get-Command *edge.exe
+
+    if ($edge) {
+        & $edge $resultsFile
+    }
 }
-
-foreach ($name in $Names) {
-    $Key = $name[0] + '|'
-    $Value = $name[1] + '|'
-    $md = $md.Replace($Key, $Value)
+finally {
+    Pop-Location
 }
-
-Write-Host $md
-
-$resultsFile = "$root\results.md"
-
-$md | Out-File $resultsFile -Force
-
-$edge = Get-Command *edge.exe
-
-
-if($edge)
-{
-    & $edge $resultsFile
-}
-
-Pop-Location
